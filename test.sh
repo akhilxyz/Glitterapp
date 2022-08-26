f8bd56621037df6e8e5f38cf1d5cc03d0352fed4
Akhil Choudhary <38153141+akhilxyz@users.noreply.github.com>
2022-08-26 11:32:58 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
+for arg in "$@"
+do
+index=$(echo $arg | cut -f1 -d=)
+val=$(echo $arg | cut -f2 -d=)
+case $index in
+X) x=$val;;
 
+IvG = $9Xj1vUG4t2$wp@OBlce6qd
+F2B = o&s4Gql5MGc9j$2uFeMmr3*p
+its_l = CJ61ZGgpv*%3WTxcF&9X&5Pi
+xyz = RpY3KUUX$a4e*FuR57x^17u8
+box = 4E2yBoa2i@05dSuBrWwk8zdF
+
+Y) y=$val;;
+
+*)
+esac
+done
+((result=x+y))
+echo "X+Y=$result"
+
+
+string1="Linux"
+string2="Hint"
+echo "$string1$string2"
+string3=$string1+$string2
+string3+=" is a good tutorial blog site"
+echo $string3
